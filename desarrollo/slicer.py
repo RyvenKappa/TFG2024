4e682206b3789d0c5e66704a7b415891555ee57a
Diego Aceituno Seoane <diego99aceituno@gmail.com>
2024-02-29 10:29:16 +0100

    "29-2"

--- desarrollo/slicer.py
+++ desarrollo/slicer.py
@@ -4,35 +4,37 @@ import numpy as np
 video_stream = cv2.VideoCapture("prueba.MTS")
 
 ok, img = video_stream.read()
-
+img = img[0:1080,0:1550]
+img1 = img.copy()
 while ok:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray,(5,5),0)
-
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    gray = cv2.convertScaleAbs(gray,1.5,1)
+    
+    
     ret,thresh = cv2.threshold(gray,50,255,0)
+    thresh = cv2.bitwise_not(thresh)
     c,hierarchy = cv2.findContours(thresh, 1, 2)
     contours = sorted(c, key=cv2.contourArea,reverse=True)[:10]
 
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area<100000 and area>1000000:
-            try:
-                contours.remove(contour)
-            except:
-                None
-
+    
+    rectangles = []
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area>150000 and area<1000000:
+        if area>115000 and area<350000:
             x,y,w,h = cv2.boundingRect(cnt)
             ratio = float(w)/h
-            if ratio>=0.5 and ratio <= 1.5:
+            if ratio>=0.6 and ratio <= 0.9:
                 rect = cv2.minAreaRect(cnt)
                 box = cv2.boxPoints(rect)
                 box = np.intp(box)
-                img = cv2.drawContours(img,[box],0,(0,0,255),2)
+                rectangles.append(box)
+    if len(rectangles)>=0:
+        img = cv2.drawContours(thresh,rectangles,-1,(0,0,255),2)
+        rectangles.clear()
 
     cv2.imshow("Shapes", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     ok,img = video_stream.read()
+    img = img[0:1080,0:1550]
---
+++ pruebas/background_substraction.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+
+
+backSub = cv.createBackgroundSubtractorMOG2()
+backSub2 = cv.createBackgroundSubtractorKNN()
+capture = cv.VideoCapture("prueba.MTS")
+ret, frame = capture.read()
+frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+fondo = frame.copy()
+kernel = cv.getStructuringElement(cv.MORPH_RECT,(2,2))
+
+while True:
+    ret, frame = capture.read()
+    frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    if frame is None:
+        break
+    fgMask = backSub2.apply(frame)
+
+    fgMask = cv.morphologyEx(fgMask,cv.MORPH_CLOSE,kernel)
+    
+    cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+    cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+    
+    
+    cv.imshow('FG Mask', fgMask)
+    
+    keyboard = cv.waitKey(1)
+    if keyboard == 'q' or keyboard == 27:
+        break
