aac651551969ee3ffb3204d148aca1353e55091a
Diego Aceituno Seoane <diego99aceituno@gmail.com>
2024-03-06 16:26:04 +0100

    "Módulo para pruebas de Background substraction con guardado para probar "

--- pruebas/background_substraction.py
+++ pruebas/background_substraction.py
@@ -4,20 +4,23 @@ import cv2 as cv
 backSub = cv.createBackgroundSubtractorMOG2()
 backSub2 = cv.createBackgroundSubtractorKNN()
 capture = cv.VideoCapture("prueba.MTS")
-ret, frame = capture.read()
-frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+
+#Añadido para guardar video de background substracting
+out = cv.VideoWriter("NoBackground.avi",int(cv.VideoWriter.fourcc(*'XVID')),25.0,(1920,1080),isColor=False)
+
+ret, frame_or = capture.read()
+frame = cv.cvtColor(frame_or,cv.COLOR_BGR2GRAY)
 fondo = frame.copy()
 kernel = cv.getStructuringElement(cv.MORPH_RECT,(2,2))
 
 while True:
-    ret, frame = capture.read()
-    frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    ret, frame_or = capture.read()
+    frame = cv.cvtColor(frame_or,cv.COLOR_BGR2GRAY)
     if frame is None:
         break
     fgMask = backSub2.apply(frame)
-
     fgMask = cv.morphologyEx(fgMask,cv.MORPH_CLOSE,kernel)
-    
+    out.write(fgMask)
     cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
     cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
                cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
@@ -26,5 +29,9 @@ while True:
     cv.imshow('FG Mask', fgMask)
     
     keyboard = cv.waitKey(1)
-    if keyboard == 'q' or keyboard == 27:
-        break
+    #if cv.waitKey(1) & 0xFF == ord('q'):
+    #    print("hola3")
+    #    out.release()
+
+out.release()
+capture.release()
