58aba4532082eb2d047fe8688dc14a5d2836c24d
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-03-14 11:06:17 +0100

    "Pruebas de yolo+pytorch,SAM"

--- desarrollo/contornos.py
+++ desarrollo/contornos.py
@@ -1,5 +1,29 @@
 import cv2
 import numpy as np
+from torch import cuda
+from ultralytics import YOLO
+model = YOLO("yolov8n.pt") 
+model.info()
+model.to('cuda')
+results = model.predict(source="test.mp4",show=True)
+# from ultralytics import SAM
 
-def contornos(img):
-    0
+# # Load a model
+# model = SAM('sam_b.pt')
+# model.to('cuda')
+
+# # Display model information (optional)
+# model.info()
+
+# model.predict(source="test.mp4",show=True)
+
+# from ultralytics import FastSAM
+
+# # Load a model
+# model = FastSAM('FastSAM-s.pt')
+# model.to('cuda')
+
+# # Display model information (optional)
+# model.info()
+
+# model.predict(source="test.mp4",show=True)
