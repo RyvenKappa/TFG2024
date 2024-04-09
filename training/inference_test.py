23c390d52dac33b82e60384d97ec161a337f41f1
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-04-09 12:33:12 +0200

    "Añadido probado de guardado"

--- training/inference_test.py
+++ training/inference_test.py
@@ -2,4 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("C:/Users/Diego/Documents/TFG2024/runs/detect/train/weights/best.pt")
 model.to("cuda")
-results = model.predict(source="testCompleto.mov",show=True)
+#Con el argumento save podemos decirle que guarde el video o las imágenes según si es solo un fotograma
+#Con save_crop le decimos que guarde todos los recordes de todas las bounding box, sin información añadida y sin poder acceder al bucle
+results = model.predict(source="test2.mp4",save=True,save_crop=True)
