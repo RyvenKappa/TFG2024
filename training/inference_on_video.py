cca2e4940e35c9f67a1f0c7b67309a0a0af32240
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-04-30 12:33:40 +0200

    "Commit antiguo"

--- training/inference_on_video.py
+++ training/inference_on_video.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 import matplotlib.patches as mpatches
-model = YOLO("runs/detect/train/weights/best.pt")
+model = YOLO("runs/detect/train8/weights/best.pt")
 
 #model = YOLO("runs/detect/train/weights/best.onnx")
 #Con el argumento save podemos decirle que guarde el video o las imágenes según si es solo un fotograma
 #Con save_crop le decimos que guarde todos los recordes de todas las bounding box, sin información añadida y sin poder acceder al bucle
-results = model.predict(source="resources/videos/23_NT_R1_J1_P9_10.mp4",save=True)
+results = model.predict(source="resources/videos/trim.mp4",save=True)
--- training/training_test.py
+++ training/training_test.py
@@ -3,7 +3,7 @@ def main():
     from ultralytics import YOLO
     model = YOLO("yolov8n.pt")
     model.to('cuda')
-    model.train(data='C:/Users/Diego/Documents/TFG2024/training/sp.yaml',epochs=100,imgsz=640,device='0',batch=-1,cache=False)
+    model.train(data='C:/Users/Diego/Documents/TFG2024/training/sp.yaml',epochs=24,imgsz=640,device='0',batch=-1,cache=False)
 
 if __name__=="__main__":
     main()
