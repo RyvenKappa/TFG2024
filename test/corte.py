89397c6ffe4c7fd8cdc640aea09ba3abc89cd6d0
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-05-04 13:14:58 +0200

    "MoviePy para guardar"

--- test/corte.py
+++ test/corte.py
@@ -1,5 +1,7 @@
 from moviepy.editor import *
 
-clip = VideoFileClip("prueba.MTS")
+clip = VideoFileClip("C:/Users/Diego/Documents/Código/TFG2024/resources/videos/prueba.MTS")
 
-clip = clip.subclip(27,60)
+clip = clip.subclip(410,486)
+
+clip.write_videofile("targetTestExp2.MTS",fps=25)
