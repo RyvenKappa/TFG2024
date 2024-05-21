c66c6f9e565f1aef7251487f3b5e23e38428fcf6
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-05-21 12:04:56 +0200

    "Arreglado deteccion con OBB"

--- src/YOLOv8Model.py
+++ src/YOLOv8Model.py
@@ -3,10 +3,12 @@ Modulo que define una interfaz util para cargar, predecir y obtener los resultad
 @author: Diego Aceituno Seoane
 @Mail: diego.aceituno@alumnos.upm.es
 """
-from ModelVersion import Version
 import torch
 import openvino as ov
 from ultralytics import YOLO
+from sys import getsizeof
+import pandas as pd
+
 class Yolo_Model:
 
 
@@ -20,10 +22,10 @@ class Yolo_Model:
         cuda_bool = torch.cuda.is_available()
         path = "src/models/normal/" #Path donde se encuentran los modelos, TODO ser capaz de buscarlos/descargarlos del repositorio de Diego
         self.task="detect"
-        self.last_prediction_results=None
+        self.last_prediction_results=pd.DataFrame()
         if obb == True:
             path = "src/models/obb/"
-            task="obb"
+            self.task="obb"
         try:
             if cuda_bool:
                 print(f"Modelo a cargar de pt,gpu=",torch.get_device(0))
@@ -51,18 +53,26 @@ class Yolo_Model:
         """
             Método para realizar inferencia sobre un video y obtener resultados
         """
+        #self.last_prediction_results.clear()
         if source==None:
             raise Exception("No se ha indicado el path del video")
         try:
             resultados = self.model.predict(source=source,save=save,stream=True) #Returns a results generator with stream=True
             for r in resultados:
-                continue
-            pass
-        except:
+                data = []
+                data.append(r.boxes)
+                data.append(r.obb)
+                data.append(r.probs)
+            print(getsizeof(self.last_prediction_results))
+        except Exception as e:
             raise Exception(f"Problema en la inferencia sobre video:",str(source))
+    def get_boxes_results(self):
+        data = pd.DataFrame(self.last_prediction_results)
+        return data
     
     
 
 if __name__ == '__main__':
-    modelo = Yolo_Model()
-    modelo.video_inference(source="resources/videos/23_NT_R1_J1_P9_10.mp4",save=True)
+    modelo = Yolo_Model(obb=True)
+    modelo.video_inference(source="resources/videos/23_NT_R1_J1_P9_10.mp4")
+    data = modelo.get_boxes_results()
--- training/inference_on_video.py
+++ training/inference_on_video.py
@@ -8,4 +8,4 @@ model = YOLO("runs/obb/train10/weights/best_openvino_model")
 #model = YOLO("runs/detect/train/weights/best.onnx")
 #Con el argumento save podemos decirle que guarde el video o las imágenes según si es solo un fotograma
 #Con save_crop le decimos que guarde todos los recordes de todas las bounding box, sin información añadida y sin poder acceder al bucle
-results = model.predict(source="resources/videos/23_NT_R1_J7_P9_P10.mp4",save=False)
+results = model.predict(source="resources/videos/23_NT_R1_J1_P9_10.mp4",save=False)
