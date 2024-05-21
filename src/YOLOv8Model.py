034c9707437c5c637d318e88c86224db0c4caff9
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-05-21 12:43:27 +0200

    "Added model swap without creating new Yolo_Model object"

--- src/YOLOv8Model.py
+++ src/YOLOv8Model.py
@@ -17,15 +17,60 @@ class Yolo_Model:
         Configura y carga el modelo
         """
         self.model = None
+        if obb == True:
+            self.task = "obb"
+        else:
+            self.task="detect"
+        self.last_prediction_results=None
+        self.__set_model()
+        
+
+
+    def video_inference(self,source = None,save = False):
+        """
+            Método para realizar inferencia sobre un video y obtener resultados
+        """
+        if source==None:
+            raise Exception("No se ha indicado el path del video")
+        try:
+            resultados = self.model.predict(source=source,save=save,stream=True) #Returns a results generator with stream=True
+            data = []
+            if self.task == "obb":
+                for r in resultados:
+                    data.append(r.obb)
+            else:
+                for r in resultados:
+                    data.append(r.boxes)
+            self.last_prediction_results = pd.DataFrame(data)
+            #print(getsizeof(self.last_prediction_results))
+        except Exception as e:
+            raise Exception(f"Problema en la inferencia sobre video:",str(source))
+        
+    def get_boxes_results(self):
+        return self.last_prediction_results
+    
+    def set_task(self,task):
+        if task not in ["detect","obb"]:
+            raise Exception(f"El tipo de trabajo ({task}) que se esta pidiendo no es el correcto.")
+        else:
+            if task != self.task:
+                print(f"Se va a cambiar a un modo de trabajo {task}")
+                self.task = task
+                self.__set_model()
+            else:
+                print("Ya esta activo este tipo de trabajo")
+
+    def __set_model(self):
+        """
+            Metodo privado de la clase que se usa para configurar el modelo según hardware y según el tipo de trabajo marcado en el objeto
+        """
         core = ov.Core()
         devices1 = core.available_devices
         cuda_bool = torch.cuda.is_available()
         path = "src/models/normal/" #Path donde se encuentran los modelos, TODO ser capaz de buscarlos/descargarlos del repositorio de Diego
-        self.task="detect"
-        self.last_prediction_results=pd.DataFrame()
-        if obb == True:
+        
+        if self.task == "obb":
             path = "src/models/obb/"
-            self.task="obb"
         try:
             if cuda_bool:
                 print(f"Modelo a cargar de pt,gpu=",torch.get_device(0))
@@ -48,33 +93,16 @@ class Yolo_Model:
             print("Problema cargando un modelo eficiente, cargando modelo por defecto tipo PyTorch")
             self.model = YOLO(f"{path}best.pt",task=self.task)
 
-
-    def video_inference(self,source = None,save = False):
-        """
-            Método para realizar inferencia sobre un video y obtener resultados
-        """
-        if source==None:
-            raise Exception("No se ha indicado el path del video")
-        try:
-            resultados = self.model.predict(source=source,save=save,stream=True) #Returns a results generator with stream=True
-            data = []
-            if self.task == "obb":
-                for r in resultados:
-                    data.append(r.obb)
-            else:
-                for r in resultados:
-                    data.append(r.boxes)
-            self.last_prediction_results = pd.DataFrame(data)
-            #print(getsizeof(self.last_prediction_results))
-        except Exception as e:
-            raise Exception(f"Problema en la inferencia sobre video:",str(source))
-    def get_boxes_results(self):
-        return self.last_prediction_results
-    
+        
     
 
 if __name__ == '__main__':
     modelo = Yolo_Model(obb=False)
+    modelo.set_task("detect")
+    modelo.set_task("obb")
     modelo.video_inference(source="resources/videos/23_NT_R1_J1_P9_10.mp4")
     data = modelo.get_boxes_results()
-    print("hola")
+    modelo.set_task("detect")
+    modelo.video_inference(source="resources/videos/23_NT_R1_J1_P9_10.mp4")
+    data = modelo.get_boxes_results()
+    
