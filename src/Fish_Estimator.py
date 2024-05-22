3dbeaf23f40a68f2493d64041a6752316b6145f0
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-05-22 13:22:00 +0200

    "Added type checking of results.boxes or results.OBB in the fish estimation"

--- src/Fish_Estimator.py
+++ src/Fish_Estimator.py
@@ -2,25 +2,25 @@
 Modulo que contiene funciones para estimar el numero de peces
 """
 import pandas as pd
-from sklearn.cluster import KMeans
 import numpy as np
+import ultralytics.engine.results
 
-def estimate_fish_number(data:pd.DataFrame,type = "detect"):
+def estimate_fish_number(data:pd.DataFrame):
     """
     Recibo un array con las cajas, imagen original y el shape original.
     A través de las cajas y el shape original analizaremos cuantos peces hay
-    @returns True si hay 2
-    @returns False si hay 1
+    @returns 2 si hay 2
+    @returns 1 si hay 1
     """
     samples = []
     img_x = data[2][0][1]
     data = data[0] #Sacamos la serie que representa la columna de bounding boxes
-    if type == "detect":
+    if type(data[0][0]) == ultralytics.engine.results.Boxes:
         for d in data:
             xywh = d.xywh #tensor
             for value in xywh:
                 samples.append(value[0])
-    elif type == "obb":
+    elif type(data[0][0]) == ultralytics.engine.results.OBB:
         for d in data:
             xywhr = d.xywhr #tensor
             for value in xywhr:
@@ -28,20 +28,19 @@ def estimate_fish_number(data:pd.DataFrame,type = "detect"):
 
     #Clustering by KMeans is done to obtain the number of bounding boxes for the x
     x2 = []
+    samples_mean = np.mean(samples)
     for s in samples:
-        x2.append(abs(s-(img_x/2)))
+        x2.append(abs(s-samples_mean))
     deviation = np.mean(x2)
+    print(deviation)
+    print(samples_mean)
     if deviation>=img_x/6:
         #Hay 2 peces
         print("Hay 2 truchas")
-        return True
-        #x = np.array(samples)
-        #x = x.reshape(-1,1)
-        #kmeans = KMeans(n_clusters=2,random_state=0).fit(x)
-        #print(kmeans.cluster_centers_)
+        return 2,samples_mean
     else:
         print("Hay 1 trucha")
-        return False
+        return 1
     
 
 
