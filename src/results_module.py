f2cdc218f991dcef119dc32a211ab3269e81ba7c
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-06-17 18:20:46 +0200

    "Obtención básica de resultados y estimación de peces a los 50 frames"

--- src/results_module.py
+++ src/results_module.py
@@ -1,15 +1,26 @@
-
-from collections.abc import Callable
 from multiprocessing import Process
-from typing import Any, Iterable, Mapping
 import pandas as pd
+from Fish_Estimator import estimate_fish_number
 
 class Data_Processor(Process):
 
-    def __init__(self,recepcion_endpoint):
+    def __init__(self,recepcion_endpoint,update_endpoint):
         super().__init__()
         self.data_enpoint = recepcion_endpoint
+        self.update_enpoint = update_endpoint
+        self.datos_entrantes = []
+        self.trout_number = None
+        self.mean = None
 
     def run(self):
+        frame = 0
         while True:
             datos = self.data_enpoint.recv()
+            self.update_enpoint.send(frame)#Envio el frame por el que voy
+            if datos is not None:
+                frame = frame + 1
+                self.datos_entrantes.append(datos)
+            if len(self.datos_entrantes)==50 and self.trout_number==None:
+                self.trout_number,self.mean = estimate_fish_number(pd.DataFrame(self.datos_entrantes))
+                print(f"Tenemos {self.trout_number} truchas y una mediana de: {self.mean}")
+
