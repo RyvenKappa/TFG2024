d9cd3d79ac13fa1ed84ecf022793b21690b28e07
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-06-23 21:25:39 +0200

    "Explicación en el manual de la ventana de datos realizada, imagenes añadidas para esto"

--- src/config_dpg.py
+++ src/config_dpg.py
@@ -10,10 +10,18 @@ def set_config():
     #Cargado de imagenes dinámicas
     width, height, channels, data = dpg.load_image("src/images/Gamma.png")
     width2, height2, channels2, data2 = dpg.load_image("src/images/LoadingScreen.png")
+    width3, height3, channels3, data3 = dpg.load_image("src/images/DataWindowManual.png")
+    width4, height4, channels4, data4 = dpg.load_image("src/images/Graficas1DataManual.png")
+    width5, height5, channels5, data5 = dpg.load_image("src/images/Graficas2DataManual.png")
+    width6, height6, channels6, data6 = dpg.load_image("src/images/EdicionDataManual.png")
     #width1,height1,channels1,data1 = dpg.load_image("src/images/pescado.gif")
     with dpg.texture_registry(show=False):
         dpg.add_static_texture(width=width, height=height, default_value=data, tag="texturaGamma")
         dpg.add_static_texture(width=width2, height=height2, default_value=data2, tag="texturaLoadingManual")
+        dpg.add_static_texture(width=width3, height=height3, default_value=data3, tag="DataCompletaManual")
+        dpg.add_static_texture(width=width4, height=height4, default_value=data4, tag="Graficas1Manual")
+        dpg.add_static_texture(width=width5, height=height5, default_value=data5, tag="Graficas2Manual")
+        dpg.add_static_texture(width=width6, height=height6, default_value=data6, tag="EdicionManual")
         dpg.add_raw_texture(width=720,height=480,default_value=np.ones((480*720*3)),tag="VideoTexture",format=dpg.mvFormat_Float_rgb)
         #dpg.add_dynamic_texture(width=width1, height=height1, default_value=data1, tag="pescadoGirando")
     
